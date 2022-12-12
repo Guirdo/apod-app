b0c72f1e435503420a77b57675c50c8c6fac9392
Guirdo <produccionesaldo@gmail.com>
2022-12-12 17:54:41 -0600

    feat: :sparkles: add modifyValues for modifying the inital state of the useForm hook

--- hooks/useForm.js
+++ hooks/useForm.js
@@ -3,6 +3,10 @@ import {useState} from 'react';
 export const useForm = (initialState = {}) => {
   const [values, setValues] = useState(initialState);
 
+  const modifyValues = (values) => {
+    setValues(values);
+  };
+
   const reset = () => {
     setValues(initialState);
   };
@@ -16,5 +20,5 @@ export const useForm = (initialState = {}) => {
     });
   };
 
-  return [values, handleInputChange, reset];
+  return [values, handleInputChange, modifyValues, reset];
 };
