b3987932ece4698408669e2ab570f7a8de01f6d7
Guirdo <produccionesaldo@gmail.com>
2022-12-12 17:14:03 -0600

    feat: :sparkles: add fetchAPOD function in the picture of the day component

--- components/PictureOfTheDay.jsx
+++ components/PictureOfTheDay.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import useAPODStore from '../stores/apod';
 import DatePicker from './DatePicker';
 import Slider from './Slider';
 
 // eslint-disable-next-line require-jsdoc
 function PictureOfTheDay() {
+  const fetchAPOD = useAPODStore((state) => state.fetchAPOD);
+
+  useEffect(() => {
+    fetchAPOD();
+  });
+
   return (
     <main className='main'>
       <DatePicker />
