d1c7c2ebb67d5e0868acb394d7b191f42b8eea6d
Guirdo <produccionesaldo@gmail.com>
2022-12-12 15:53:34 -0600

    chore: :lipstick: add sldier styles

--- components/PictureOfTheDay.jsx
+++ components/PictureOfTheDay.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import DatePicker from './DatePicker';
+import Slider from './Slider';
 
 // eslint-disable-next-line require-jsdoc
 function PictureOfTheDay() {
   return (
     <main className='main'>
       <DatePicker />
+      <Slider />
     </main>
   );
 }
