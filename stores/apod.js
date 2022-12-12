174a621fdc334bcbbd922a6ecf2b3bb8410888ed
Guirdo <produccionesaldo@gmail.com>
2022-12-12 17:09:18 -0600

    feat: :sparkles: add general store for all components

---
+++ stores/apod.js
@@ -0,0 +1,30 @@
+import moment from 'moment';
+import create from 'zustand';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+const useAPODStore = create((set, get) => ({
+  copyright: '',
+  date: moment().format('YYYY-MM-DD'),
+  explanation: '',
+  hdurl: '#',
+  media_type: 'image',
+  title: 'Loading',
+  url: '#',
+  fetchAPOD: async () => {
+    const date = get().date;
+    const response = await fetch(`${API_URL}&date=${date}`)
+        .then((res) => res.json());
+
+    set({...response});
+  },
+  fetchAPODWithDate: async (date) => {
+    const response = await fetch(`${API_URL}&date=${date}`)
+        .then((res) => res.json());
+
+    set({...response});
+  },
+}));
+
+export default useAPODStore;
