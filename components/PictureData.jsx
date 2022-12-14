0d4004c89beedb5dc3d36993c587596006e64efa
Guirdo <produccionesaldo@gmail.com>
2022-12-14 12:02:35 -0600

    feat: :sparkles: add PIctureData component

---
+++ components/PictureData.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import useAPODStore from '../stores/apod';
+
+function PictureData() {
+  const title = useAPODStore((state) => state.title);
+  const explanation = useAPODStore((state) => state.explanation);
+  const copyright = useAPODStore((state) => state.copyright);
+  const isLoading = useAPODStore((state)=> state.isLoading);
+
+  return (
+    <div className='picture-data'>
+      {
+        isLoading ?
+        <h2>Loading...</h2> :
+        <>
+          <h2 className='picture__title'>{title}</h2>
+          <p className='picture__explanation'>{explanation}</p>
+          <p className='picture__copyright'>
+            <span className='picture__label'>Credits: </span>
+            {copyright || 'No data'}
+          </p>
+        </>
+      }
+    </div>
+  );
+}
+
+export default PictureData;
--- components/PictureOfTheDay.jsx
+++ components/PictureOfTheDay.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import useAPODStore from '../stores/apod';
 import DatePicker from './DatePicker';
+import PictureData from './PictureData';
 import Slider from './Slider';
 
 // eslint-disable-next-line require-jsdoc
@@ -15,6 +16,7 @@ function PictureOfTheDay() {
     <main className='main'>
       <DatePicker />
       <Slider />
+      <PictureData />
     </main>
   );
 }
