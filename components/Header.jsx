ad931471c6db31079e15472be4b2aa9b675a0c05
Guirdo <produccionesaldo@gmail.com>
2022-12-14 14:02:45 -0600

    chore: :lipstick: improve Header layout for more components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable require-jsdoc */
 import React from 'react';
+import QualitySwitch from './QualitySwitch';
 
 function Header() {
   return (
     <header className="header">
       <span className="header__brand">Astronomical Picture Of the Day</span>
+
+      <QualitySwitch />
     </header>
   );
 }
