9a863f91568496af657c4e23166fb45568f1e659
Guirdo <produccionesaldo@gmail.com>
2022-12-17 12:39:11 -0600

    feat: :sparkles: add router for turning the project into a single page application

--- components/PictureOfTheDay.jsx
+++ components/PictureOfTheDay.jsx
@@ -13,11 +13,11 @@ function PictureOfTheDay() {
   });
 
   return (
-    <main className='main'>
+    <>
       <DatePicker />
       <Slider />
       <PictureData />
-    </main>
+    </>
   );
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 /* eslint-disable require-jsdoc */
 import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Header from '../components/Header';
 import PictureOfTheDay from '../components/PictureOfTheDay';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PictureOfTheDay />,
+  },
+  {
+    path: '/random',
+    element: <h1>Random page</h1>,
+  },
+]);
 
 function App() {
   return (
     <>
       <Header />
-      <PictureOfTheDay />
+      <main className='main'>
+        <RouterProvider router={router} />
+      </main>
     </>
   );
 }
