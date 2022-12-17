c893196eb6ff3eb0259dc36dc3c72e7a5d1b8c7c
Guirdo <produccionesaldo@gmail.com>
2022-12-17 13:31:22 -0600

    feat: :sparkles: add responsiveness to the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable require-jsdoc */
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import cx from 'classnames';
+import {Cancel, Menu} from 'iconoir-react';
 import QualitySwitch from './QualitySwitch';
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+  const itemClasses = cx('navbar__item', {'navbar__item--active': isActive});
+
   return (
     <nav className="navbar">
       <ul className='navbar-list'>
@@ -15,7 +20,7 @@ function Navbar() {
             Astronomical Picture Of the Day
           </Link>
         </li>
-        <li className='navbar__item'>
+        <li className={itemClasses}>
           <Link
             className='navbar__link'
             to='/random'
@@ -23,8 +28,20 @@ function Navbar() {
             Random picture
           </Link>
         </li>
-        <li className='navbar__item'>
+        <li className={itemClasses}>
           <QualitySwitch />
+        </li>
+        <li
+          className="navbar__toggle"
+          onClick={() => setIsActive(!isActive)}
+        >
+          {
+            !isActive ? (
+              <span><Menu strokeWidth={2} /></span>
+            ) : (
+              <span><Cancel strokeWidth={2} /></span>
+            )
+          }
         </li>
       </ul>
     </nav>
