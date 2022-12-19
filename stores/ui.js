8418fc5a23610f64af2ebafaf49364aa5cf0c0f5
Guirdo <produccionesaldo@gmail.com>
2022-12-19 13:51:35 -0600

    feat: :sparkles: add errorMessage in the UI store

--- stores/ui.js
+++ stores/ui.js
@@ -2,7 +2,9 @@ import create from 'zustand';
 
 const useUIStore = create((set) => ({
   isHighQuality: false,
+  errorMessage: '',
   setIsHighQuality: (value) => set({isHighQuality: value}),
+  setErrorMessage: (message) => set({errorMessage: message}),
 }));
 
 export default useUIStore;
