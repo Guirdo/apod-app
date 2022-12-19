9ff19ad2dc93db6e2293af4158387aa3f5b13db0
Guirdo <produccionesaldo@gmail.com>
2022-12-19 14:20:06 -0600

    feat: :poop: add fetching date in the url query

--- components/PictureOfTheDay.jsx
+++ components/PictureOfTheDay.jsx
@@ -1,18 +1,37 @@
-import moment from 'moment';
 import React, {useEffect} from 'react';
+import moment from 'moment';
+import {useLocation, useNavigate} from 'react-router-dom';
 import useAPODStore from '../stores/apod';
 import DatePicker from './DatePicker';
 import PictureData from './PictureData';
 import Slider from './Slider';
+import isAValidDate from '../helper/isAValidDate';
+import useUIStore from '../stores/ui';
 
 // eslint-disable-next-line require-jsdoc
 function PictureOfTheDay() {
   const fetchAPOD = useAPODStore((state) => state.fetchAPOD);
-  const setDate = useAPODStore((state)=>state.setDate);
+  const fetchAPODWithDate = useAPODStore((state) => state.fetchAPODWithDate);
+  const setErrorMessage = useUIStore((state) => state.setErrorMessage);
+  const setDate = useAPODStore((state) => state.setDate);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    setDate(moment().format('YYYY-MM-DD'));
-    fetchAPOD();
+    const dateQuery = new URLSearchParams(location.search).get('date');
+    if (!dateQuery) {
+      setDate(moment().format('YYYY-MM-DD'));
+      fetchAPOD();
+    } else {
+      const dateValidation = isAValidDate(dateQuery);
+
+      if (dateValidation === true) {
+        fetchAPODWithDate(dateQuery);
+      } else {
+        setErrorMessage(dateValidation);
+        navigate('/');
+      }
+    }
   });
 
   return (
