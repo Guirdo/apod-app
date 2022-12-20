62d4a884877bb8095245fe14d911c821c5505f4d
Guirdo <produccionesaldo@gmail.com>
2022-12-20 13:31:44 -0600

    feat: :sparkles: add showVideo variable to the UI store

--- stores/ui.js
+++ stores/ui.js
@@ -3,8 +3,10 @@ import create from 'zustand';
 const useUIStore = create((set) => ({
   isHighQuality: false,
   errorMessage: '',
+  showVideo: true,
   setIsHighQuality: (value) => set({isHighQuality: value}),
   setErrorMessage: (message) => set({errorMessage: message}),
+  setShowVideo: (value) => set({showVideo: value}),
 }));
 
 export default useUIStore;
